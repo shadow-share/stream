cffe65f67d737bd2567bf93bea1108d46b6aa26f
JShadowMan <jshadowman@163.com>
2017-07-02 00:06:39 +0800

    Bits and Byte

--- stream.py
+++ stream.py
@@ -35,8 +35,11 @@ class NotImplement(Exception):
 
 
 def _object_to_string(class_object, **kwargs):
+    name = class_object if isinstance(class_object, str) else \
+        class_object.__class__.__name__
+
     return '<{} {}>'.format(
-        class_object.__class__.__name__.strip('_'),
+        name.strip('_').capitalize(),
         ' '.join([
             '{}={}'.format(k.capitalize(), v) for k, v in kwargs.items()]))
 
@@ -80,19 +83,24 @@ class _Bits(ctypes.Structure):
     """
 
     _fields_ = [
-        ('b7', ctypes.c_uint8, 1), ('b6', ctypes.c_uint8, 1),
-        ('b5', ctypes.c_uint8, 1), ('b4', ctypes.c_uint8, 1),
-        ('b3', ctypes.c_uint8, 1), ('b2', ctypes.c_uint8, 1),
-        ('b1', ctypes.c_uint8, 1), ('b0', ctypes.c_uint8, 1)
+        ('b0', ctypes.c_uint8, 1), ('b1', ctypes.c_uint8, 1),
+        ('b2', ctypes.c_uint8, 1), ('b3', ctypes.c_uint8, 1),
+        ('b4', ctypes.c_uint8, 1), ('b5', ctypes.c_uint8, 1),
+        ('b6', ctypes.c_uint8, 1), ('b7', ctypes.c_uint8, 1)
     ]
 
-    def get_bits(self):
-        return [self.b0, self.b1, self.b2, self.b3,
+    def get_bits(self, str_type=False):
+        bits = [self.b0, self.b1, self.b2, self.b3,
                 self.b4, self.b5, self.b6, self.b7]
+        if str_type:
+            return ''.join([str(bit) for bit in bits])
+        return bits
 
     def __str__(self):
         return _object_to_string(
-            self, bits=''.join(str(b) for b in self._get_bits()))
+            '_Bits', bits=''.join(str(b) for b in self._get_bits()))
+
+    __repr__ = __str__
 
 
 class _Byte(ctypes.Union):
@@ -111,7 +119,6 @@ class _Byte(ctypes.Union):
 
     _anonymous_ = ('bits',)
 
-
     def __init__(self, octet_integer):
         super(_Byte, self).__init__()
 
@@ -120,33 +127,55 @@ class _Byte(ctypes.Union):
         self.value = octet_integer & 0xff
 
     def __str__(self):
-        return _object_to_string(self, binary=bin(self.value[0]))
+        return _object_to_string(
+            '_Byte', binary=self.bits.get_bits(str_type=True), value=self.value)
+
+    __repr__ = __str__
 
 
 class _ByteArray(object):
+
+    """ Array of _Byte type
+    """
 
     def __init__(self):
         self._byte_array = list()
         self._array_size = 0
 
     def append(self, byte):
+        self._array_size += 1
+        # TODO. using dict
         if isinstance(byte, bytes):
-            self._byte_array.append(_Byte(byte[0]))
+            return self._byte_array.append(_Byte(byte[0]))
         if isinstance(byte, str):
-            self._byte_array.append(_Byte(ord(byte[0])))
+            return self._byte_array.append(_Byte(ord(byte[0])))
         if isinstance(byte, int):
-            self._byte_array.append(_Byte(byte))
-        if isinstance(byte, float):
-            self._byte_array.append(_Byte(int(byte)))
-        print(self._byte_array[0])
+            return self._byte_array.append(_Byte(byte))
+        if isinstance(byte, _Byte):
+            return self._byte_array.append(byte)
+        self._array_size -= 1
+        raise ValueError(
+            "Byte unsupported value type '{}'".format(type(byte).__name__))
 
     def pop(self, index=-1):
         return self._byte_array.pop(index)
 
-    def get_byte(self, index):
-        if index < 0 or index >= self._array_size:
-            raise IndexError('Byte index out of range')
-        return self._byte_array[index].value
+    def extend(self, data):
+        if isinstance(data, (str, bytes)):
+            for el in data:
+                self.append(el)
+        elif isinstance(data, int):
+            while data:
+                self.append(data)
+                data >>= 8
+        else:
+            raise ValueError('data except str, bytes or int')
+
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            return self._byte_array[item]
+        if isinstance(item, slice):
+            return self._byte_array.__class__(self._byte_array[item])
 
     def __len__(self):
         return len(self._byte_array)
@@ -161,7 +190,20 @@ class _ByteArray(object):
 
 
 class Packet(_ByteArray):
-    pass
+
+    """ Binary data of packet
+    """
+    
+    def __init__(self, fill=None):
+        super(Packet, self).__init__()
+        self.fill(element=fill, count=1)
+
+    def fill(self, element=None, count=1):
+        if isinstance(element, (str, bytes, int)):
+            for _ in range(count):
+                self.extend(element)
+        else:
+            raise ValueError('element except str, bytes or int type')
 
 
 class Stream(object):
@@ -177,7 +219,23 @@ class SecureStream(object):
 
 
 if __name__ == '__main__':
+    import unittest
 
-    byte_array = _ByteArray()
-    byte_array.append('\xff')
-    print(byte_array)
+    class BitsTestCase(unittest.TestCase):
+
+        def testBitsZero(self):
+            bits = _Bits(0, 0, 0, 0, 0, 0, 0, 0)
+            self.assertEqual(bits.get_bits(), [0, 0, 0, 0, 0, 0, 0, 0])
+
+        def testBitsNonZero(self):
+            bits = _Bits(1, 1, 1, 1, 1, 1, 1, 1)
+            self.assertEqual(bits.get_bits(), [1, 1, 1, 1, 1, 1, 1, 1])
+
+        def testBitsStrVal(self):
+            bits = _Bits(1, 0, 1, 0, 1, 0, 1, 0)
+            self.assertEqual(bits.get_bits(str_type=True), '10101010')
+
+    stream_test_suite = unittest.TestSuite()
+    stream_test_suite.addTest(BitsTestCase())
+
+    unittest.main()
