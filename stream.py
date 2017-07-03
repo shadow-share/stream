b300fc56746d044816ca2515e96be7e2e0378e9d
JShadowMan <jshadowman@163.com>
2017-07-03 22:23:48 +0800

    refaceor all

--- stream.py
+++ stream.py
@@ -2,240 +2,174 @@
 #
 # Copyright (C) 2017 DouLe
 #
-from __future__ import print_function, with_statement
-import ctypes
+# from __future__ import print_function, with_statement
+import time
+import array
 import socket
 import struct
+import asyncio
 
 
-""" This module provides more powerful streaming data service based on socket.
-
-
-"""
-
-__all__ = ['Packet', 'Stream', 'SecureStream']
-
-
-""" Exceptions Definition """
-
-
-class IllegalParameter(Exception):
+class IllegalValue(Exception):
     pass
 
 
-class StreamError(Exception):
+class SteamError(Exception):
     pass
 
 
-class NotImplement(Exception):
+class FamilyInvalid(Exception):
     pass
 
 
-""" Internal Methods """
+BIG_ENDING = '>'
+LITTLE_ENDING = '<'
+NATIVE_BYTE_ORDER = LITTLE_ENDING
+if struct.pack('@h', 0x0102) == b'\x01\x02':
+    NATIVE_BYTE_ORDER = BIG_ENDING
 
 
-def _object_to_string(class_object, **kwargs):
-    name = class_object if isinstance(class_object, str) else \
-        class_object.__class__.__name__
+class Packet(object):
 
-    return '<{} {}>'.format(
-        name.strip('_').capitalize(),
-        ' '.join([
-            '{}={}'.format(k.capitalize(), v) for k, v in kwargs.items()]))
+    def __init__(self, encoding='utf-8'):
+        self._byte_array = array.array('B')
+        self._encoding = encoding
 
+    def fill(self, contents):
+        if isinstance(contents, str):
+            return self._byte_array.fromstring(contents)
+        if isinstance(contents, bytes):
+            return self._byte_array.frombytes(contents)
+        raise IllegalValue('fill method except str or bytes parameter')
 
-def _string_cast(_object, encoding='utf-8'):
-    """ Convert Any type object to str type
-    """
-    if isinstance(_object, str):
-        return _object
-    if isinstance(_object, bytes):
-        return _object.decode(encoding)
-    return str(_object)
+    def put_uint8(self, value):
+        self._byte_array.append(value & 0xff)
 
+    def put_uint16(self, value):
+        self.put_uint8((value & 0xff00) >> 8)
+        self.put_uint8(value & 0xff)
 
-def _bytes_cast(_object, encoding='utf-8'):
-    """ Convert Any type object to bytes type
-    """
-    if isinstance(_object, bytes):
-        return _object
-    if isinstance(_object, str):
-        return _object.encode(encoding)
-    return bytes(_object)
+    def put_uint32(self, value):
+        self.put_uint16((value & 0xffff0000) >> 16)
+        self.put_uint16(value & 0xffff)
 
+    def put_uint64(self, value):
+        self.put_uint32((value & 0xffffffff00000000) >> 32)
+        self.put_uint32(value & 0xffffffff)
 
-""" Internal Classes """
+    def pop_uint8(self):
+        return self._byte_array.pop()
 
+    def pop_uint16(self):
+        # 0x1234   --->   0x12, 0x34 [Big-Ending]
+        # POP: 0x34 | 0x12 << 8
+        return self.pop_uint8() | self.pop_uint8() << 8
 
-class _Bits(ctypes.Structure):
-    """ C Structure
+    def pop_uint32(self):
+        # 0x11223344   --->   0x11, 0x22, 0x33, 0x44
+        # POP: 0x44 | 0x33 << 8 | 0x22 << 16 | 0x11 << 24
+        return self.pop_uint16() | self.pop_uint16() << 16
 
-    typedef struct {
-        unsigned b0 : 1;
-        unsigned b1 : 1;
-        unsigned b2 : 1;
-        unsigned b3 : 1;
-        unsigned b4 : 1;
-        unsigned b5 : 1;
-        unsigned b6 : 1;
-        unsigned b7 : 1;
-    } _Bits;
-    """
+    def pop_uint64(self):
+        # 0x1122334455667788 --> 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88
+        # POP: 0x88 | 0x77 << 8 | 0x66 << 16 | ... | 0x11 < 56
+        return self.pop_uint32() | self.pop_uint32() << 32
 
-    _fields_ = [
-        ('b0', ctypes.c_uint8, 1), ('b1', ctypes.c_uint8, 1),
-        ('b2', ctypes.c_uint8, 1), ('b3', ctypes.c_uint8, 1),
-        ('b4', ctypes.c_uint8, 1), ('b5', ctypes.c_uint8, 1),
-        ('b6', ctypes.c_uint8, 1), ('b7', ctypes.c_uint8, 1)
-    ]
+    def put_string(self, string):
+        self.fill(string)
 
-    def get_bits(self, str_type=False):
-        bits = [self.b0, self.b1, self.b2, self.b3,
-                self.b4, self.b5, self.b6, self.b7]
-        if str_type:
-            return ''.join([str(bit) for bit in bits])
-        return bits
+    def pop_string(self, length):
+        string = self._byte_array.tobytes()[len(self) - length:]
+        for _ in range(length):
+            self._byte_array.pop()
+        return string
 
-    def __str__(self):
-        return _object_to_string(
-            '_Bits', bits=''.join(str(b) for b in self._get_bits()))
+    def pop_all(self):
+        return self.pop_string(len(self))
 
-    __repr__ = __str__
+    def to_bytes(self):
+        return self._byte_array.tobytes()
 
-
-class _Byte(ctypes.Union):
-    """ C union
-    
-    typedef union {
-        _Bits bits;
-        unsigned char value;
-    } Byte;
-    """
-
-    _fields_ = [
-        ('value', ctypes.c_char),
-        ('bits', _Bits)
-    ]
-
-    _anonymous_ = ('bits',)
-
-    def __init__(self, octet_integer):
-        super(_Byte, self).__init__()
-
-        if not isinstance(octet_integer, int):
-            raise IllegalParameter('Internal error of _Byte constructor')
-        self.value = octet_integer & 0xff
-
-    def __str__(self):
-        return _object_to_string(
-            '_Byte', binary=self.bits.get_bits(str_type=True), value=self.value)
-
-    __repr__ = __str__
-
-
-class _ByteArray(object):
-
-    """ Array of _Byte type
-    """
-
-    def __init__(self):
-        self._byte_array = list()
-        self._array_size = 0
-
-    def append(self, byte):
-        self._array_size += 1
-        # TODO. using dict
-        if isinstance(byte, bytes):
-            return self._byte_array.append(_Byte(byte[0]))
-        if isinstance(byte, str):
-            return self._byte_array.append(_Byte(ord(byte[0])))
-        if isinstance(byte, int):
-            return self._byte_array.append(_Byte(byte))
-        if isinstance(byte, _Byte):
-            return self._byte_array.append(byte)
-        self._array_size -= 1
-        raise ValueError(
-            "Byte unsupported value type '{}'".format(type(byte).__name__))
-
-    def pop(self, index=-1):
-        return self._byte_array.pop(index)
-
-    def extend(self, data):
-        if isinstance(data, (str, bytes)):
-            for el in data:
-                self.append(el)
-        elif isinstance(data, int):
-            while data:
-                self.append(data)
-                data >>= 8
-        else:
-            raise ValueError('data except str, bytes or int')
+    def exists(self, subject):
+        if isinstance(subject, int):
+            if self._byte_array.count(subject):
+                return True
+        if isinstance(subject, str):
+            subject = subject.encode(self._encoding)
+        if isinstance(subject, bytes):
+            origin = self._byte_array.tobytes()
+            if origin.find(subject) != -1:
+                return True
+        return False
 
     def __getitem__(self, item):
         if isinstance(item, int):
-            return self._byte_array[item]
+            return self._byte_array.__getitem__(item)
         if isinstance(item, slice):
-            return self._byte_array.__class__(self._byte_array[item])
+            return [byte for byte in self._byte_array.__getitem__(item)]
+        if isinstance(item, tuple) and len(item) == 2:
+            _bytes = self.__getitem__(item[0])
+            if not isinstance(_bytes, list):
+                _bytes = [_bytes]
+
+            return [
+                tuple(
+                    (int(b) for b in bin(byte)[2:].rjust(8, '0'))
+                ).__getitem__(item[1]) for byte in _bytes]
+        raise IllegalValue('Packet indices except integers or slices and tuple')
 
     def __len__(self):
         return len(self._byte_array)
 
     def __str__(self):
-        return _object_to_string(self, length=len(self))
-
-    __repr__ = __str__
+        return '<Packet Length={}>'.format(len(self))
 
 
-""" Publish Classes """
+AF_INET = socket.AF_INET
+AF_INET6 = socket.AF_INET6
 
 
-class Packet(_ByteArray):
+class Steam(object):
 
-    """ Binary data of packet
-    """
-    
-    def __init__(self, fill=None):
-        super(Packet, self).__init__()
-        self.fill(element=fill, count=1)
+    def __init__(self, family=AF_INET):
+        if family not in [AF_INET, AF_INET6]:
+            raise FamilyInvalid('family only defined AF_INET and AF_INET6')
+        self._socket_fd = socket.socket(family)
+        self._buffer = Packet()
 
-    def fill(self, element=None, count=1):
-        if isinstance(element, (str, bytes, int)):
-            for _ in range(count):
-                self.extend(element)
-        else:
-            raise ValueError('element except str, bytes or int type')
+    def connect(self, host, port, block=False):
+        try:
+            self._socket_fd.connect((host, port))
+            self._socket_fd.setblocking(block)
+        except ConnectionRefusedError:
+            raise SteamError('No connection could be made '
+                             'because the target machine actively refused it')
+
+    def send(self, data):
+        if isinstance(data, Packet):
+            data = data.to_bytes()
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+        if not isinstance(data, bytes):
+            raise IllegalValue('data except str, bytes or Packet type')
+        print(self._socket_fd.send(data))
+
+    def receive(self, length=0, *, flag=None):
+        self._fill_packet()
+        return self._buffer.pop_all()
+
+    def _fill_packet(self):
+        buffer = self._socket_fd.recv(0)
+        if buffer is None:
+            print('Socket is closed')
+        self._buffer.fill(buffer)
 
 
-class Stream(object):
-    """
-    
-    """
-    def __init__(self):
-        pass
+stream = Steam()
+stream.connect('127.0.0.1', 8080)
 
-
-class SecureStream(object):
-    pass
-
-
-if __name__ == '__main__':
-    import unittest
-
-    class BitsTestCase(unittest.TestCase):
-
-        def testBitsZero(self):
-            bits = _Bits(0, 0, 0, 0, 0, 0, 0, 0)
-            self.assertEqual(bits.get_bits(), [0, 0, 0, 0, 0, 0, 0, 0])
-
-        def testBitsNonZero(self):
-            bits = _Bits(1, 1, 1, 1, 1, 1, 1, 1)
-            self.assertEqual(bits.get_bits(), [1, 1, 1, 1, 1, 1, 1, 1])
-
-        def testBitsStrVal(self):
-            bits = _Bits(1, 0, 1, 0, 1, 0, 1, 0)
-            self.assertEqual(bits.get_bits(str_type=True), '10101010')
-
-    stream_test_suite = unittest.TestSuite()
-    stream_test_suite.addTest(BitsTestCase())
-
-    unittest.main()
+get_request = Packet()
+get_request.fill(b'GET / HTTP/1.1\r\n\r\n')
+stream.send(get_request)
+time.sleep(5)
+print(stream.receive())
